69d5e04720d782456ea7361af91f8844ece6d15d
Herman Rivas <hrivas5726@aol.com>
2019-06-18 11:07:24 -0400

    cc2

--- src/components/CourseContainer.js
+++ src/components/CourseContainer.js
@@ -11,19 +11,59 @@ class CourseContainer extends Component {
     courses: []
   }
 
+
+ changeTitle = (e) => {
+
+   let courseId = e.target.value
+   console.log("courseId:", courseId)
+   let courseTitle = this.state.courses.find(course=> {
+     return course.id === parseInt(e.target.value)
+   })
+   // console.log("checking", courseTitle)
+   this.setState({currentCourse: courseTitle})
+
+
+  //working on the 3rd deliverable
+
+
+
+   // fetch("https://bayside-high.herokuapp.com/api/v1/users/160/courses/courseId")
+   // .then(res => res.json())
+   // .then(student => {
+   //   this.setState({currentStudent: student})
+   // })
+
+ }
+
+  componentDidMount() {
+    fetch("https://bayside-high.herokuapp.com/api/v1/users/160/courses/")
+    .then(res => res.json())
+    .then(courses => {
+      this.setState({courses: courses})
+    })
+
+
+  }
+
+
+
   render() {
+    console.log("in CourseCont", this.state.courses)
+    // successfully fetched data
+    console.log("students", this.state.currentStudent)
+
     return (
       <div className="ui grid container">
         <div className="ui center aligned header sixteen wide column">
-          {/* Course Title Here */}
+          {this.state.currentCourse.name}
           Course Title
         </div>
 
-        <CourseSelector />
+        <CourseSelector courses={this.state.courses} changeTitle={this.changeTitle}/>
 
         <EditStudent />
 
-        <StudentsList />
+        <StudentsList currentStudents={this.state.currentStudent} />
       </div>
     )
   }
